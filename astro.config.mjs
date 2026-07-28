669c226c5766d4e69d750b6bfe76a3128b2d7126
federodrigo <frodrigo@gmail.com>
2026-07-28 08:30:28 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   site: "https://minimaltolerance.com",
+  trailingSlash: "always",
+
   integrations: [mdx(), sitemap()],
+
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
